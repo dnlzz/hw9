5216d6068753001b37e096479134b4ff01f5067d
Tony <adeglio@gmail.com>
2015-11-25 15:01:24 -0500

    just need injector

--- hw9.sh
+++ hw9.sh
@@ -6,7 +6,7 @@ while(($hr < 16)); do
     cdate=`date +%Y_%d_%m_%H_%M_%S`
     echo $cdate
     fn="nyse_${cdate}.html"
-    wget -O $fn http://www.wsj.com/mdc/public/page/2_3021-activnyse-actives.html
+    wget -O $fn http://www.wsj.com/mdc/public/page/2_3021-activnyse-actives.html -P home/tony/Documents/NJIT/CS-288/hw9/htmldocs
     sleep 30
     hr=`date +%H`
 done
