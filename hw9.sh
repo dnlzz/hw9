ab8c98c366f2e77ff0b0ff36c2f8bacbe7da2466
Tony <adeglio@gmail.com>
2015-11-25 14:12:24 -0500

    need injector

--- hw9.sh
+++ hw9.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-cdate=`date +%Y_%d_%m_%H_%M_%S`
+hr=`date +%H`
 
-fn="nyse_${cdate}.html"
-
-wget -O $fn http://www.wsj.com/mdc/public/page/2_3021-activnyse-actives.html
-
-#java -jar tagsoup-1.2.1.jar --files $fn
+while(($hr < 16)); do
+    cdate=`date +%Y_%d_%m_%H_%M_%S`
+    echo $cdate
+    fn="nyse_${cdate}.html"
+    wget -O $fn http://www.wsj.com/mdc/public/page/2_3021-activnyse-actives.html
+    sleep 30
+    hr=`date +%H`
+done
 
